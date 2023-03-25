7e587b118ed43a395fae7648dc51b91abc0c55d0
Prashar4801 <Prashar4801@gmail.com>
2023-03-25 19:13:02 +0000

    Added a Browse Items Page.
    The following commit:
    - Adds mock data for the application
    - Allows browsing of products
    - Sets up ViewModela dn Model folders

--- Artistic/ViewModel/BrowseItemsViewModel.cs
+++ Artistic/ViewModel/BrowseItemsViewModel.cs
@@ -1,14 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using  Artistic.Model;
+using System.ComponentModel;
 
 namespace Artistic.ViewModel;
 
+using System.Reflection;
+
 public partial class BrowseItemsViewModel : ObservableObject
 {
+    [ObservableProperty]
+    List<Item> items;
+
+    public BrowseItemsViewModel()
+    {
+        LoadItems();
+    }
+    private void LoadItems()
+    {
+
+        Items = new()
+        {
+            new Item(
+
+                Name: "Photograph of Watch",
+                ProductImage: ImageSource.FromFile("C:\\Users\\Priya\\OneDrive\\Documents\\6002CEM\\Artistic\\Resources\\SampleItems\\pexels_cottonbro_studio.jpg"),
+                Description: "Photograph of a black watch",
+                Artist: "Joey Nguyen",
+                UploadTime: DateTime.UtcNow.AddDays(1)
+            ),
+            new Item(
+                Name: "Photograph of Phone",
+                ProductImage: ImageSource.FromResource("Artistic.Resources.SampleItems.pexels_cottonbro_studio.jpg", typeof(BrowseItemsViewModel).GetTypeInfo().Assembly),
+                Description: "Photograph of a Phone",
+                Artist: "Gantas Valciulenas",
+                UploadTime: DateTime.UtcNow.AddDays(3).AddHours(2)
+            )
+        };
+
+    }
 
 }
 
