5650f8d324c75bd04a87960a6b492a658e78dcc4
Prashar4801 <Prashar4801@gmail.com>
2023-03-27 00:00:43 +0100

    Added Flyout for Navigation
    The following commit:
    - Adds the necessary code to allow navigation using a flyout.
    - Adds missing file for background

--- Artistic/MainPage.xaml.cs
+++ Artistic/MainPage.xaml.cs
@@ -5,13 +5,12 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-
     }
 
     async void LoginButton(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new BrowseItems());
+        await Shell.Current.GoToAsync($"//{nameof(BrowseItems)}");
     }
 
+   
 }
-
